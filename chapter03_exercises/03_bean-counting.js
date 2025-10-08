da572101c5ea627317929623d8d57e54f62f247d
jsonsDev <183668194+jsonsDev@users.noreply.github.com>
2025-10-09 03:09:26 +0700

    Rewrite countBs function to use countChar function.

--- chapter03_exercises/03_bean-counting.js
+++ chapter03_exercises/03_bean-counting.js
@@ -1,21 +1,15 @@
-function countBs(string) {
-    let bCount = 0;
-    for (let i = 0; i < string.length; i ++) {
-        if (string[i] === "B") {
-            bCount += 1;
-        }
-    }
-    return bCount;
-}
-
 function countChar(string, char) {
     let charCount = 0;
-    for (let i = 0; i < string.length; i ++) {
+    for (let i = 0; i < string.length; i++) {
         if (string[i] === char) {
             charCount += 1;
         }
     }
     return charCount;
+}
+
+function countBs(string) {
+    return countChar(string, "B");
 }
 
 // Eloquent JavaScript provided logs:
