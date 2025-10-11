78a8d8ab0184d99dcb99a80642d66644e5bd1918
jsonsDev <183668194+jsonsDev@users.noreply.github.com>
2025-10-11 16:07:51 +0700

    Write reverseArrayInPlace function.

--- chapter04_exercises/02_reversing-an-array.js
+++ chapter04_exercises/02_reversing-an-array.js
@@ -1,5 +1,26 @@
 function reverseArray(myArray) {
     let newArray = [];
+    
     for (let i = myArray.length -1; i >= 0; i--) newArray.push(myArray[i]);
     return newArray;
 } 
+
+function reverseArrayInPlace(myArray) {
+    let arrayLength = myArray.length;
+    
+    for (let i = myArray.length -1; i >= 0; i--) myArray.push(myArray[i]);
+    for (let i = 0; i < arrayLength; i++) myArray.shift(myArray[i]);
+    return myArray;
+}
+
+
+// Eloquent JavaScript provided logs:
+let myArray = ["A", "B", "C"];
+console.log(reverseArray(myArray));
+// → ["C", "B", "A"];
+console.log(myArray);
+// → ["A", "B", "C"];
+let arrayValue = [1, 2, 3, 4, 5];
+reverseArrayInPlace(arrayValue);
+console.log(arrayValue);
+// → [5, 4, 3, 2, 1]
