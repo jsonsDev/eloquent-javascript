84714abe65a31499a79ef394d78e5bff9329b4b7
jsonsDev <183668194+jsonsDev@users.noreply.github.com>
2025-10-13 19:30:51 +0700

    Write deepEqual function.

---
+++ chapter04_exercises/04_deep-comparison.js
@@ -0,0 +1,26 @@
+function deepEqual(a, b) {
+    if (a === b) return true;
+    
+    if (typeof a !== "object" || typeof b !== "object" || a == null || b == null) {
+        return false;
+    }
+
+    let aKeys = Object.keys(a), bKeys = Object.keys(b);
+
+    if (aKeys.length !== bKeys.length) return false;
+
+    for (let key of aKeys) {
+        if (!bKeys.includes(key) || !deepEqual(a[key], b[key])) return false;
+    }
+
+    return true;
+}
+
+// Eloquent JavaScript provided logs:
+let obj = {here: {is: "an"}, object: 2};
+console.log(deepEqual(obj, obj));
+// → true
+console.log(deepEqual(obj, {here: 1, object: 2}));
+// → false
+console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
+// → true
