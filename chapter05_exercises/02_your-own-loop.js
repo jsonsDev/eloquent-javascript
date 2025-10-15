d30d75a204b6504dd1f968655633097063e85626
jsonsDev <183668194+jsonsDev@users.noreply.github.com>
2025-10-15 12:10:50 +0700

    Write higher-order function loop.

---
+++ chapter05_exercises/02_your-own-loop.js
@@ -0,0 +1,11 @@
+function loop(value, test, update, body) {
+    for (let i = value; test(i); i = update(i)) {
+        body(i);
+    }
+}
+
+// Eloquent Javascript provided:
+loop(3, n => n > 0, n => n - 1, console.log);
+// → 3
+// → 2
+// → 1
